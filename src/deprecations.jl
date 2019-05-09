4daa03f5e464f44c63d192b0405d6528271b9ced
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-09 10:00:49 +0000

    Snapshot of branch master taken on 2019-05-09T06:00:47.153-04:00 from "https://github.com/UnofficialJuliaMirror/Yao.jl-5872b779-8223-5990-8dd0-5abbb0748c8c"

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -1,4 +1,4 @@
-using YaoBase
+using YaoBase, YaoBlocks, YaoArrayRegister
 using BitBasis: BitStr
 # NOTE: this file should only exists in v0.4.x
 #       since in v0.4.x the conponent packages
@@ -6,8 +6,8 @@ using BitBasis: BitStr
 #       deprecation warning itself.
 
 # Base
-@deprecate addbit!(r::AbstractRegister, n::Int) increase!(r, n)
-@deprecate addbit!(n::Int) increase!(n)
+@deprecate addbit!(r::AbstractRegister, n::Int) addbits!(r, n)
+@deprecate addbit!(n::Int) addbits!(n)
 @deprecate reset!(r::AbstractRegister; val::Integer=0)  collapseto!(r, val)
 @deprecate measure_reset!(r::AbstractRegister; val::Int=0) measure_collapseto!(r; config=val)
 @deprecate measure_reset!(r::AbstractRegister, locs; val::Int=0) measure_collapseto!(r, locs; config=val)
@@ -20,6 +20,25 @@ using BitBasis: BitStr
 
 @deprecate usedbits(block::AbstractBlock{N}) where N occupied_locs(block)
 
+################ Compatibility Code ###################
+export DefaultRegister, MatrixBlock, Sequential, ReflectBlock, GeneralMatrixGate, AddBlock, ⊗
+const DefaultRegister = ArrayReg
+const MatrixBlock = AbstractBlock
+const Sequential = ChainBlock
+const ReflectBlock = ReflectGate
+const GeneralMatrixGate = GeneralMatrixBlock
+const AddBlock = YaoBlocks.Sum
+
+@deprecate sequence(args...) chain(args...)
+@deprecate matrixgate(args...) matblock(args...)
+
+import Base: collect, parent
+@deprecate collect(t::Type, x::AbstractBlock) collect_blocks(t, x)
+
+# joining two registers
+⊗(reg::AbstractRegister, reg2::AbstractRegister) = join(reg, reg2)
+⊗(A::AbstractArray, B::AbstractArray) = kron(A, B)
+
 # Originally addrs means return the addression stored in a composite block
 # this is not necessary since not all composite block store the address, e.g
 # roller, etc.
@@ -27,5 +46,18 @@ using BitBasis: BitStr
 
 # NOTE: block is frequently used as variable name, to make sure there is
 #       no conflicts, this is commented.
-# @deprecate block(x::AbstractContainer) = parent(x)
-# @deprecate chblock(x::AbstractContainer, blk::AbstractBlock) = chsubblocks(x, blk)
+@deprecate block(x::AbstractContainer) content(x)
+@deprecate parent(x::AbstractContainer) content(x)
+@deprecate chblock(x::AbstractContainer, blk::AbstractBlock) chsubblocks(x, blk)
+
+@deprecate timeevolve(block::MatrixBlock, t::Number; tol::Real=1e-7) time_evolve(block, t; tol=tol)
+@deprecate timeevolve(t::Number; tol::Real=1e-7) time_evolve(t; tol=tol)
+
+
+import Base: collect, *
+@deprecate collect(x::AbstractBlock, ::Type{T}) where {T <: AbstractBlock} collect_blocks(T, x)
+
+function Base.:*(op::AbstractMatrix, r::ArrayRegOrAdjointArrayReg)
+    Base.depwarn("`matrix * register` is deprecated", :*)
+    return op * state(r)
+end
